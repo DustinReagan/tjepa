327d3d81685f9642076c7177239929d2174806a1
Dustin Reagan <drreagan@gmail.com>
2024-03-17 12:43:15 -0500

    1d conversion

--- src/masks/random.py
+++ src/masks/random.py
@@ -1,67 +1,46 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-#
-
 from multiprocessing import Value
-
 from logging import getLogger
-
 import torch
 
 _GLOBAL_SEED = 0
 logger = getLogger()
 
-
 class MaskCollator(object):
-
     def __init__(
         self,
         ratio=(0.4, 0.6),
-        input_size=(224, 224),
-        patch_size=16,
+        input_length=224,  # Assuming input_length is the total number of patches in 1D
+        patch_size=16,  # Assuming patch_size still relevant for 1D sequence length
     ):
         super(MaskCollator, self).__init__()
-        if not isinstance(input_size, tuple):
-            input_size = (input_size, ) * 2
         self.patch_size = patch_size
-        self.height, self.width = input_size[0] // patch_size, input_size[1] // patch_size
+        self.length = input_length // patch_size  # Sequence length in patches for 1D
         self.ratio = ratio
         self._itr_counter = Value('i', -1)  # collator is shared across worker processes
 
     def step(self):
-        i = self._itr_counter
-        with i.get_lock():
-            i.value += 1
-            v = i.value
-        return v
+        with self._itr_counter.get_lock():
+            self._itr_counter.value += 1
+            return self._itr_counter.value
 
     def __call__(self, batch):
         '''
-        Create encoder and predictor masks when collating imgs into a batch
-        # 1. sample enc block (size + location) using seed
-        # 2. sample pred block (size) using seed
-        # 3. sample several enc block locations for each image (w/o seed)
-        # 4. sample several pred block locations for each image (w/o seed)
-        # 5. return enc mask and pred mask
+        Create encoder and predictor masks when collating items into a batch for 1D input
+        # 1. sample block size using seed and specified ratio
+        # 2. return enc mask and pred mask
         '''
         B = len(batch)
-
         collated_batch = torch.utils.data.default_collate(batch)
 
         seed = self.step()
         g = torch.Generator()
         g.manual_seed(seed)
-        ratio = self.ratio
-        ratio = ratio[0] + torch.rand(1, generator=g).item() * (ratio[1] - ratio[0])
-        num_patches = self.height * self.width
+        ratio = self.ratio[0] + torch.rand(1, generator=g).item() * (ratio[1] - self.ratio[0])
+        num_patches = self.length
         num_keep = int(num_patches * (1. - ratio))
 
         collated_masks_pred, collated_masks_enc = [], []
         for _ in range(B):
-
             m = torch.randperm(num_patches)
             collated_masks_enc.append([m[:num_keep]])
             collated_masks_pred.append([m[num_keep:]])
